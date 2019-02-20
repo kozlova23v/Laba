1db9720ecfe09e8549997852ba76c4fddf6df23a
kozlova23v <47798558+kozlova23v@users.noreply.github.com>
2019-02-20 04:02:43 +0300

    Add files via upload

---
+++ L_1.dpr
@@ -0,0 +1,15 @@
+program L_1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+  writeln('Hello, World from Delphi!');
+  writeln('This is the console application');
+  writeln;
+  writeln('Press <Enter> to quit');
+  readln;
+end.
